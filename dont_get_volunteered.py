105f184123dec2fa9494eb96cd8c6406522644f0
Jason J <36523652+jjason685@users.noreply.github.com>
2018-06-07 09:00:12 -0400

    Update dont_get_volunteered.py

--- dont_get_volunteered.py
+++ dont_get_volunteered.py
@@ -1,5 +1,5 @@
 def answer(src, dest):
-    board = [[0 for i in range(8)] for j in range(8)]
+    board = [[0 for i in range(10)] for j in range(10)]
     fillBoard(board, src, 0)
     if src == dest:
         return 0
@@ -11,12 +11,19 @@ def fillBoard(board, currentPos, numOfMoves):
     
     possibleRes = [1,2,2,1,2,-1,1,-2,-1,-2,-2,-1,-2,1,-1,2]
     
-    i = 0
-    while i < 16:
+    for i in range(0, 16, 2):
         newx = currentx + possibleRes[i]
         newy = currenty + possibleRes[i+1]
         if newx >= 0 and newy >= 0 and newx < 8 and newy < 8:
             if board[newx][newy] == 0 or board[newx][newy] > numOfMoves+1:
                 board[newx][newy] = numOfMoves+1
                 fillBoard(board, newx * 8 + newy, numOfMoves+1)
-        i+=2
+
+#print(answer(19,36)) test case
+#print(answer(0,1)) test cast
+#print(answer(0,9)) test case
+
+"""
+code fully working, not brute forcing (recursion would take too long) but instead finding the distance to each point
+on the board by pointing in 2d arr
+"""
